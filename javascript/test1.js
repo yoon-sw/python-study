db5540715455721618964af581d385f4177aeeba
ps98k@naver.com <ps98k@naver.com>
2021-05-13 16:57:59 +0900

    5.13

---
+++ javascript/test1.js
@@ -0,0 +1,65 @@
+
+//1. 자바스크립트 타입
+// var n1 = 10;
+// var n2 = 10.5;
+// var s1 = "문자열";
+// var b1 = true;
+
+// console.log(n1);
+// console.log(n2);
+// console.log(s1);
+// console.log(b1);
+
+//2.자바스크립트 오브젝트 타입
+// var board = {
+//     id: 1,
+//     title: "제목",
+//     content: "내용1",
+//     friend: {
+//         username: "홍홍홍"
+//     }
+// };
+
+// console.log(board);
+// console.log(board.title);
+// console.log(board.friend.username);
+
+
+//3.자바스크립트 함수
+function add() {
+    return 10;
+}
+
+var result = add();
+console.log(result);
+
+
+//4. 익명함수
+var m = function () {
+    return 20;
+}
+
+console.log(m());
+
+// 5.화살표 함수
+var r = () => 30;  // =>는 function 생략, {} 생략, 자동 return
+
+console.log(r());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
